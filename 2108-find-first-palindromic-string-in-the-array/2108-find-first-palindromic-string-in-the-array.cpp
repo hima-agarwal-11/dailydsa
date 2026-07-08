bd3c9101c29b13962ccbf13bda9bc0ab706ca854
HIMA AGARWAL <hima.24bce10633@vitbhopal.ac.in>
2026-07-08 22:26:41 +0530

    Time: 6 ms (41.4%), Space: 29.8 MB (26.31%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string firstPalindrome(vector<string>& words) {
+
+        for (string word : words) {
+            if (isPalindrome(word))
+                return word;
+        }
+
+        return "";
+    }
+};
