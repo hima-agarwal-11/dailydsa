bd2f34e893d0b7a311f933a2c00d797d13000616
HIMA AGARWAL <hima.24bce10633@vitbhopal.ac.in>
2026-07-26 00:00:09 +0530

    Time: 0 ms (100%), Space: 9.3 MB (68.64%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> mp;
+        for(char ch:s){
+            mp[ch]++;
+        }
+        int ans=0;
+        bool hasodd=false;
+        for(auto it : mp){
+            if(it.second%2==0){
+                ans+=it.second;
+        }
+        else{
+            ans+=it.second-1;
+            hasodd=true;
+        }
+        }
+        if (hasodd)
+        ans++;
+        return ans;
+    }
+
+};
+
+
+      
