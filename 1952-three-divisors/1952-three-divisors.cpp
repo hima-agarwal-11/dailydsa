bf6d665bc19d67ee7318617319665bad0de55934
HIMA AGARWAL <hima.24bce10633@vitbhopal.ac.in>
2026-08-14 23:06:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (58.67%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPrime(int x) {
+        if (x < 2)
+            return false;
+
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    bool isThree(int n) {
+        int root = sqrt(n);
+
+        if (root * root != n)
+            return false;
+
+        return isPrime(root);
+    }
+};
