a41ddd1359d354d17ef1457da8770cc8b7986c27
HIMA AGARWAL <hima.24bce10633@vitbhopal.ac.in>
2026-09-05 21:39:37 +0530

    Time: 19 ms (25.11%), Space: 210.9 MB (35.81%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>maxarr(n);
+        vector<int>minarr(n);
+        int maxi= nums[0];
+            for (int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+            maxarr[i] = maxi;
+        }
+        int mini=nums[n-1];
+        for (int i = n - 1; i >= 0; i--) {
+            mini = min(mini, nums[i]);
+            minarr[i] = mini;
+        }
+        for (int i = 0; i < n; i++) {
+            if (maxarr[i] - minarr[i] <= k)
+                return i;
+        }
+        return -1;
+    }
+};
