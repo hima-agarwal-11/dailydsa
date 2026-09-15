7248ee99e6325ff6caeae32895d9d946fd0dd269
HIMA AGARWAL <hima.24bce10633@vitbhopal.ac.in>
2026-09-15 22:37:19 +0530

    Time: 0 ms (100%), Space: 10 MB (0%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        return rec1[2] > rec2[0] &&//left right 
+               rec2[2] > rec1[0] &&
+               rec1[3] > rec2[1] &&
+               rec2[3] > rec1[1];//top
+    }
+};
+//1------3
+//0------2
