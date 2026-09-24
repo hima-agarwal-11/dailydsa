fc67c912557e084b75f710b671fc11730e450424
HIMA AGARWAL <hima.24bce10633@vitbhopal.ac.in>
2026-09-24 14:00:55 +0530

    Time: 0 ms (100%), Space: 30.9 MB (52.36%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int digsum(int n ){
+    int sum=0;
+    while(n>0){
+        sum+=n%10;
+        n=n/10;
+    }return sum;
+}
+    int smallestIndex(vector<int>& nums) {
+        for(int i = 0 ;i<nums.size();i++){
+            if((digsum(nums[i]))==i)
+            return i;
+        }return -1;
+    }
+};
